1db743dfdd9f8ddbc315c1a3e76953fd8df4acec
Jadwiga Bochinska <matuzalema@gmail.com>
2019-01-03 11:17:12 +0100

    Try to bug carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// ====================== mustache ======================
+var templateSlide = document.querySelector('.container');
+var templateCarousel = document.querySelector('.main-carousel');
+var slideItems = '';
+Mustache.parse(templateSlide);
+
+for(var i=0; i<carouselData.length; i++){
+	slideItems += Mustache.render(templateSlide, carouselData[i]);
+	}
+
+templateCarousel.insertAdjacentHTML('beforeend', slideItems);
+
 // ============= carousel (external js: flickity.pkgd.js) ======
 
 var elem = document.querySelector('.main-carousel');
@@ -22,3 +34,15 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+// ====================== map ============================
+
+// window.initMap = function() {
+//   // The location of Uluru
+//   var uluru = {lat: -25.344, lng: 131.036};
+//   // The map, centered at Uluru
+//   var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 4, center: uluru});
+//   // The marker, positioned at Uluru
+//   var marker = new google.maps.Marker({position: uluru, map: map});
+// }
